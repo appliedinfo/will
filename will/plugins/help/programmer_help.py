10623da50fb227571f9d3a7a51931c52a892fcf7
dinowais <owais@trialx.com>
2016-08-24 14:26:01 +0530

    - removed 30 min plugin

--- will/plugins/help/programmer_help.py
+++ will/plugins/help/programmer_help.py
@@ -14,10 +14,10 @@ class ProgrammerHelpPlugin(WillPlugin):
 
         self.say(help_text, message=message)
 
-    @periodic(minute='1')
-    def standup(self):
-        self.say("hi hello this is periodic testing in help plugin for 1 min ")
-
-    @periodic(minute='30')
-    def standup(self):
-        self.say("hi hello this is  periodic testing in help plugin for 30 min ")
+    # @periodic(minute='1')
+    # def standup(self):
+    #     self.say("hi hello this is periodic testing in help plugin for 1 min ")
+    #
+    # @periodic(minute='30')
+    # def standup(self):
+    #     self.say("hi hello this is  periodic testing in help plugin for 30 min ")
