bc3bae015b12fee93db3009586a00c67c1fdc062
dinowais <owais@trialx.com>
2016-09-07 10:48:51 +0530

    -added time in custom

--- will/plugins/fitbot/custom_messages.py
+++ will/plugins/fitbot/custom_messages.py
@@ -47,8 +47,8 @@ class CustomPluginBot(WillPlugin):
 
         self.say(activity_text, message=message)
 
-    @periodic(hour='1', minute='00')
-    def schedule_530_message(self):
+    @periodic(hour='13', minute='00')
+    def schedule_1300_message(self):
         context = group_stats()
         self.say("@all Group Stats", notify=True, color='green')
         self.say(rendered_template("group_stats.html", context), notify=True,
